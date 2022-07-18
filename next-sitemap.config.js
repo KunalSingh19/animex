5f0b602daa977b74fb1e679b58f72985efe145be
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2022-07-18 20:24:42 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://watch.legendtoon.tk',
+  siteUrl: 'https://watch.legendtoon.tk/recentlyadded/1',
   generateRobotsTxt: true,
   sitemapSize: 7000,
 }
