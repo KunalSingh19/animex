93a1ee9cd4e28c9700ee0ef12815e2cfdb3cf09b
KunalSingh19 <73868343+KunalSingh19@users.noreply.github.com>
2022-07-19 16:32:17 +0530

    Delete next-sitemap.config.js

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // see https://github.com/iamvishnusankar/next-sitemap
-  siteUrl: 'https://watch.legendtoon.tk/recentlyadded/1',
-  generateRobotsTxt: true,
-  sitemapSize: 7000,
-}
